bd5bbe675301a10a6411618a115f0c1ff23b131d
Ruslan Yanbarisov <ruslan.yanbarisov@gmail.com>
2017-09-26 18:18:57 +0300

    Completed iteration 1: added overloaded log functions + testing.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -9,6 +9,22 @@ public class Logger {
         System.out.println("primitive: " + message);
     }
 
+    public static void log(boolean message) {
+        System.out.println("primitive: " + message);
+    }
+
+    public static void log(char message) {
+        System.out.println("char: " + message);
+    }
+
+    public static void log(String message) {
+        System.out.println("string: " + message);
+    }
+
+    public static void log(Object message) {
+        System.out.println("reference: " + message);
+    }
+
     public static void main(String[] args) {
         System.out.println("Hi");
     }
